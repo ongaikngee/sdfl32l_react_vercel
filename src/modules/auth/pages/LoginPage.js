a52a7c7448d7bf017aea012134923381038b87a9
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2024-06-20 11:10:28 +0800

    loginPage Responsive

--- src/modules/auth/pages/LoginPage.js
+++ src/modules/auth/pages/LoginPage.js
@@ -14,8 +14,81 @@ const LoginPage = () => {
                 <p>{AUTH_INTRO}
                 </p>
             </Container>
-            <Segment color={COLORS.semantic_primary}>
-                <h1>hello</h1>
+            <Grid>
+                <GridRow columns={1} only='mobile'>
+                    <Segment color={COLORS.semantic_primary}>
+                        <Grid>
+                            <GridRow>
+                                <GridColumn>
+                                    <Form onSubmit={loginUser}>
+                                        <FormGroup>
+                                            <FormInput
+                                                label='Username'
+                                                name='username'
+                                                placeholder='Enter Username'
+                                                error={errors.username && { content: errors.username }}
+                                            />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <FormInput
+                                                label='Password'
+                                                name='password'
+                                                type="password"
+                                                placeholder='Enter Password'
+                                                error={errors.password && { content: errors.password }}
+                                            />
+                                        </FormGroup>
+                                        <Button compact color={COLORS.semantic_primary} type='submit'>Submit</Button>
+                                    </Form>
+                                </GridColumn>
+                            </GridRow>
+                        </Grid>
+                    </Segment>
+                </GridRow>
+
+                <GridRow columns={1} only='mobile'>
+                    <GridColumn verticalAlign='middle'>
+                        <Header as='h4' color={COLORS.semantic_primary}>{APP_TITLE}</Header>
+                        {LOREM_IPSUM.paragraph}
+                    </GridColumn>
+                </GridRow>
+                <GridRow columns={2} only='tablet computer'>
+                    <h1>This is computer</h1>
+                    <Segment color={COLORS.semantic_primary}>
+                        <Grid columns={2} relaxed='very' stackable>
+                            <GridColumn>
+                                <Form onSubmit={loginUser}>
+                                    <FormGroup>
+                                        <FormInput
+                                            label='Username'
+                                            name='username'
+                                            placeholder='Enter Username'
+                                            error={errors.username && { content: errors.username }}
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <FormInput
+                                            label='Password'
+                                            name='password'
+                                            type="password"
+                                            placeholder='Enter Password'
+                                            error={errors.password && { content: errors.password }}
+                                        />
+                                    </FormGroup>
+                                    <Button compact color={COLORS.semantic_primary} type='submit'>Submit</Button>
+                                </Form>
+                            </GridColumn>
+
+                            <GridColumn verticalAlign='middle'>
+                                <Header as='h4' color={COLORS.semantic_primary}>{APP_TITLE}</Header>
+                                {LOREM_IPSUM.paragraph}
+                            </GridColumn>
+                        </Grid>
+                        <Divider vertical color={COLORS.semantic_primary}>Or</Divider>
+                    </Segment>
+                </GridRow>
+            </Grid>
+            {/* <Segment color={COLORS.semantic_primary}>
                 <Grid columns={2} relaxed='very' stackable>
                     <GridColumn>
                         <Form onSubmit={loginUser}>
@@ -45,9 +118,71 @@ const LoginPage = () => {
                         {LOREM_IPSUM.paragraph}
                     </GridColumn>
                 </Grid>
-
                 <Divider vertical color={COLORS.semantic_primary}>Or</Divider>
-            </Segment>
+            </Segment> */}
+            {/* <Segment>
+                <Grid>
+                    <GridRow columns={1} only='mobile'>
+                        <GridColumn>
+                            <Form onSubmit={loginUser}>
+                                <FormGroup>
+                                    <FormInput
+                                        label='Username'
+                                        name='username'
+                                        placeholder='Enter Username'
+                                        error={errors.username && { content: errors.username }}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <FormInput
+                                        label='Password'
+                                        name='password'
+                                        type="password"
+                                        placeholder='Enter Password'
+                                        error={errors.password && { content: errors.password }}
+                                    />
+                                </FormGroup>
+                                <Button compact color={COLORS.semantic_primary} type='submit'>Submit</Button>
+                            </Form>
+                        </GridColumn>
+                    </GridRow>
+                    <GridRow columns={1} only='mobile'>
+                        <GridColumn verticalAlign='middle'>
+                            <Header as='h4' color={COLORS.semantic_primary}>{APP_TITLE}</Header>
+                            {LOREM_IPSUM.paragraph}
+                        </GridColumn>
+                    </GridRow>
+                    <GridRow columns={2} only='tablet computer'>
+                        <GridColumn>
+                            <Form onSubmit={loginUser}>
+                                <FormGroup>
+                                    <FormInput
+                                        label='Username'
+                                        name='username'
+                                        placeholder='Enter Username'
+                                        error={errors.username && { content: errors.username }}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <FormInput
+                                        label='Password'
+                                        name='password'
+                                        type="password"
+                                        placeholder='Enter Password'
+                                        error={errors.password && { content: errors.password }}
+                                    />
+                                </FormGroup>
+                                <Button compact color={COLORS.semantic_primary} type='submit'>Submit</Button>
+                            </Form>
+                        </GridColumn>
+
+                        <GridColumn verticalAlign='middle'>
+                            <Header as='h4' color={COLORS.semantic_primary}>{APP_TITLE}</Header>
+                            {LOREM_IPSUM.paragraph}
+                        </GridColumn>
+                    </GridRow>
+                </Grid>
+            </Segment> */}
         </div>
     )
 }
