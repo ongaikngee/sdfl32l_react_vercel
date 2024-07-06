b66aab8e3c528bab68d33688ab98106880957b23
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2024-07-06 22:16:58 +0800

    error handling (#4)

--- src/modules/semantic/pages/SemanticSchoolResultPage.js
+++ src/modules/semantic/pages/SemanticSchoolResultPage.js
@@ -1,47 +1,68 @@
 import { useState, useEffect } from 'react'
-import { APP_SETTING, P1_REG_SETTING, COLORS } from '../../common/constants/common'
+import { APP_SETTING } from '../../common/constants/common'
 import { useParams } from 'react-router-dom';
+import { GridRow, GridColumn, Dimmer, Loader, Label } from 'semantic-ui-react';
 
 export default function SemanticSchoolResultPage() {
 
     const { id } = useParams(); // Get the id parameter from the URL
     const [result, setResult] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null);
     useEffect(() => {
         getAPI()
     }, [])
 
     const getAPI = async () => {
-        console.log('Come to singapore.....', id)
+        setError(null)
         setLoading(true)
         const base_url = APP_SETTING.base_url_for_backend
         const url = base_url + `p1_reg_results/${id}/`
 
-        const response = await fetch(url, {
-            method: 'GET'
-        })
-        const data = await response.json()
-
-        if (response.status === 200) {
-            console.log("getting reuslt....", data)
+        try {
+            const response = await fetch(url, {
+                method: 'GET'
+            })
+            if (!response.ok) {
+                let errorMessage = await response.text();
+                throw new Error(`Response status: ${response.status},  message: ${errorMessage}`)
+            }
+            const data = await response.json();
             setResult(data)
-        } else {
-            console.log('Something went wrong!')
+        } catch (e) {
+            setError(e.message)
+        } finally {
+            setLoading(false)
         }
     }
-
 
     return (
         <div>
             <h1>Welcome</h1>
             <h1>API Data</h1>
-            <ul>
-                {result && Object.keys(result).map(key => (
-                    <li key={key}>
-                        <strong>{key}</strong>: {result[key]}
-                    </li>
-                ))}
-            </ul>
+            {loading ? (
+                <GridRow style={{ height: '200px' }}>
+                    <GridColumn>
+                        <Dimmer active inverted>
+                            <Loader indeterminate size='large'>Getting School Listing</Loader>
+                        </Dimmer>
+                    </GridColumn>
+                </GridRow>
+            ) : (
+                <ul>
+                    {result && Object.keys(result).map(key => (
+                        <li key={key}>
+                            <strong>{key}</strong>: {result[key]}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {error && (
+                <Label basic color='red'>
+                    {error}
+                </Label>
+            )}
         </div>
     )
+
 }
